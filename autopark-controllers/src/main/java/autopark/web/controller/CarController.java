8eed06384d34a27489d80c3e9726020cfbee30b8
Test <test@test.test>
2015-12-18 21:48:18 +0300

    Добавили страницу автомобиля

--- autopark-controllers/src/main/java/autopark/web/controller/CarController.java
+++ autopark-controllers/src/main/java/autopark/web/controller/CarController.java
@@ -5,11 +5,14 @@ import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class MainPageController {
-    @RequestMapping(value = "/")
+public class CarController {
+    @RequestMapping(value = "/car")
     public String handle(ModelMap modelMap) {
-        modelMap.put("author","Петров Иван");
-        return "main.jsp";
+        modelMap.put("name","Mercedes");
+        modelMap.put("model","S600");
+        modelMap.put("year","2015");
+        modelMap.put("carcolor","черный");
+        return "car.jsp";
     }
 }
 
